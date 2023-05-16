1068140290f4c3a95fa744c17028a0ac7f5f9288
nurafansyarifudin <afan.syarifudin10@gmail.com>
2023-05-16 23:03:54 +0700

    update project directory and structure

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 
 const app = express();
+const port = 3000;
 
 app.use(express.json());
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+
+app.get("/", (req, res) => {
+    res.json({ message: "Success" });
+});
 
 app.listen(3000, () => {
-    console.log(`Server Started at ${3000}`)
+    console.log(`Server Started at ${port}`)
 })
