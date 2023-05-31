44bc1017a76c6d1902bd78b557e509b5fb8c11a9
Vanz2002 <valentinus2002@gmail.com>
2023-05-31 22:09:21 +0700

    Update Category done

--- src/controllers/categories_controller.js
+++ src/controllers/categories_controller.js
@@ -53,16 +53,35 @@ controller.createCategory = async (req, res) => {
 //update the category
 controller.updateCategory = async (req, res) => {
     try {
-        const data = await Category.update(req.body, {
+        // Check if Content-Type is set to JSON
+        if (req.headers['content-type'] !== 'application/json') {
+            return res.status(400).json({ 
+                error: 'Invalid content type. Only JSON is supported.' 
+            });
+        }
+
+        const {categoryId} = req.params;
+        const {name} = req.body;
+
+        const oldCategory = await Category.update({
+            name: name
+        }, {
             where: {
-                id: req.params.id
+                id: categoryId
             }
         });
 
-        res.status(200).json({
-            message: "Success Update Category",
-            data: data
-        })
+        if (oldCategory[0] === 1) {
+            res.status(200).json({
+                status: "Ok",
+                message: "Success Update Category"
+            })
+        } else {
+            res.status(404).json({
+                status: "Not Found",
+                message: "Category Not Found"
+            })
+        }
     } catch (error) {
         res.status(404).json({
             message: error
