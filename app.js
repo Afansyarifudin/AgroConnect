138d70bd856a838810078fee507131f1979f8f7b
Nur Afan Syarifudin <afan.syarifudin10@gmail.com>
2023-06-05 11:17:12 +0700

    Add Automation deployment pipeline

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use('/products', productRoutes);
 app.use('/demands', demandRoutes);
 
 
-// Error Handling
+// Error Handling for enpoint not found
 app.use((req, res, next) => {
     const error = new Error('Endpoint Not Found');
     error .status = 404;
