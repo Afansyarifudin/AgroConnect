3c450221e2e6d000d77eeea9a40952f1988f0539
Nur Afan Syarifudin <afan.syarifudin10@gmail.com>
2023-06-05 11:28:19 +0700

    Add Automation deployment pipeline

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const http = require('http');
 
 const app = require('./app');
-const port = process.env.APP_PORT || 3000;
+const port = 3000;
 
 const server = http.createServer(app);
 server.listen(port, ()=> {
