ec5ba69e30b9f3839e01d9e3d83b5ff09257d7be
Nur Afan Syarifudin <afan.syarifudin10@gmail.com>
2023-06-06 22:57:09 +0700

    add new avatar in model user and categories

--- src/models/user.js
+++ src/models/user.js
@@ -17,7 +17,8 @@ module.exports = (sequelize, DataTypes) => {
     username: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
-    role: DataTypes.STRING
+    role: DataTypes.STRING,
+    avatar: DataTypes.STRING
   }, {
     sequelize,
     modelName: 'User',
--- src/seeders/20230525054500-demo-category.js
+++ src/seeders/20230525054500-demo-category.js
@@ -5,32 +5,38 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     return queryInterface.bulkInsert('Categories', [
       {
-      name: "Susu",
-      createdAt: new Date(),
-      updatedAt: new Date()
-      },
-      {
       name: "Sayur",
+      avatar: "https://akcdn.detik.net.id/visual/2021/06/08/ilustrasi-sayur_169.jpeg?w=650",
       createdAt: new Date(),
       updatedAt: new Date()
       },
       {
-      name: "Serealia",
+      name: "Buah",
+      avatar: "https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_640/v1511061488/bgnpcpctorsnspupszlr.jpg",
       createdAt: new Date(),
       updatedAt: new Date()
       },
       {
-      name: "Daging",
+      name: "Rempah-Rempah",
+      avatar: "https://img.kurio.network/OPMaFNXYrkG_zmldtH49YbHB6Fc=/1200x900/filters:quality(80)/https://kurio-img.kurioapps.com/21/01/26/b4cfc046-eeb4-4acc-9268-919b99a03b2a.jpe",
       createdAt: new Date(),
       updatedAt: new Date()
       },
       {
-      name: "Ikan",
+      name: "Daging, Susu, Ikan",
+      avatar: "https://cdn2.tstatic.net/tribunkaltimtravel/foto/bank/images/daging-susu-telur-ikan.jpg",
       createdAt: new Date(),
       updatedAt: new Date()
       },
       {
-      name: "Telur",
+      name: "Padi, Jagung, Sagu",
+      avatar: "https://4.bp.blogspot.com/-TnJWd18fFM8/WJsf0PZvAEI/AAAAAAAAAcs/12vq-VL0E0YVHgx6SXIYhF0rpP49GkzIACLcB/s400/tanaman-pangan.jpg",
+      createdAt: new Date(),
+      updatedAt: new Date()
+      },
+      {
+      name: "Umbi",
+      avatar: "https://d1vbn70lmn1nqe.cloudfront.net/prod/wp-content/uploads/2022/08/26032716/X-Kandungan-Nutrisi-Umbi-umbian-yang-Baik-untuk-Kesehatan.jpg",
       createdAt: new Date(),
       updatedAt: new Date()
       }
